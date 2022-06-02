61706180b8092e1a4d7a08b80b9c5621d8be70a1
Dana-Moon <reri0312@gmail.com>
2022-06-02 19:20:44 +0900

    visual banner 변경 사항 커밋

---
+++ project/drclio/script/main_1.js
@@ -0,0 +1,34 @@
+
+    const image = document.querySelectorAll(".DB_img > a");
+    const menu = document.querySelectorAll(".DB_menu > a");
+    
+    var interval = setInterval(changeImage, 4000);
+
+    function changeImage() {
+        let i;
+        for(i=0; i<image.length; i++) {
+            if(image[i].classList.contains("selected")){
+                break;
+            }
+        }
+        image[i].classList.remove("selected");
+        menu[i].classList.remove("selected");
+        if(i==image.length-1) {
+            i=-1;
+        }
+        image[i+1].classList.add("selected");
+        menu[i+1].classList.add("selected");
+    }
+    
+    for(let i=0; i<menu.length; i++){
+        menu[i].addEventListener("click",(e)=>{
+            clearInterval(interval);
+            // console.log(menu[i]);
+            document.querySelector(".DB_menu > a.selected").classList.remove("selected");
+            document.querySelector(".DB_img > a.selected").classList.remove("selected");
+            menu[i].classList.add("selected");
+            image[i].classList.add("selected");
+            interval = setInterval(changeImage, 4000);
+        });
+    }
+    
